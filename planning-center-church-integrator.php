a3d6e3cc28c7c7191352147fa0c7243235a6f564
sagitarisandy <97603275+sagitarisandy@users.noreply.github.com>
2025-12-31 22:31:56 +0700

    Enhance class initialization and error handling in PCC_Plugin

--- planning-center-church-integrator.php
+++ planning-center-church-integrator.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: Planning Center Church Integrator
- * Version: 1.0.14
+ * Version: 1.0.15
  */
 
 if (!defined('ABSPATH')) exit;
 
-define('PCC_VERSION', '1.0.14');
+define('PCC_VERSION', '1.0.15');
 define('PCC_PLUGIN_FILE', __FILE__);
 define('PCC_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('PCC_PLUGIN_URL', plugin_dir_url(__FILE__));
